c1be21d1ca0c99c34606aced2f5858d389dec7b3
Raynur2012 <rg201288@mail.ru>
2019-04-15 13:10:01 +0300

    Доработка с учетом добавления листа и отдельных классов

--- src/ru/geekbrains/lesson_4/ChatWindow.java
+++ src/ru/geekbrains/lesson_4/ChatWindow.java
@@ -1,11 +1,21 @@
 package ru.geekbrains.lesson_4;
 
 import javax.swing.*;
+import javax.xml.soap.Text;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ChatWindow extends JFrame {
+
+    private final JPanel jp1;
+    private final TextMassageCellRenderer massageCellRenderer;
+    private final JPanel jp2;
+    private final JList<TextMassage> tList;// ответстыеннен исключительно за прорисовку элементов
+    private final DefaultListModel<TextMassage> massageListModel; //хранение и добавление новых элементов, удаление старых
+    private final TextField tf;
+    private final JButton jb;
+    private final JScrollPane scroll;
 
     public ChatWindow(){
 
@@ -15,30 +25,43 @@ public class ChatWindow extends JFrame {
         setLayout(new BorderLayout());
 
 //        создали первую панель, добавили ее по центру
-        JPanel jp1 = new JPanel();
+        jp1 = new JPanel();
         jp1.setLayout(new BorderLayout());
         add(jp1, BorderLayout.CENTER);
 
 //        создали поле для текста и добавили в первую панель
-        JTextArea ta = new JTextArea();
-        ta.setEditable(false);
-        jp1.add(ta);
+        tList = new JList<>();
+        massageListModel = new DefaultListModel<>();
+        tList.setModel(massageListModel);
+        massageCellRenderer = new TextMassageCellRenderer();
+        tList.setCellRenderer(massageCellRenderer);
+//        tList.setLineWrap(true);
+//        tList.setWrapStyleWord(true);
+//        tList.setEditable(false);
+        jp1.add(tList);
+
+        scroll = new JScrollPane(tList, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        jp1.add(scroll);
 
 //      создали вторую панель, добавили ее снизу. Изменили размер в общем BorderLayout
-        JPanel jp2 = new JPanel();
+        jp2 = new JPanel();
         jp2.setLayout(new BorderLayout());
         jp2.setPreferredSize(new Dimension(500,80));
         add(jp2, BorderLayout.SOUTH);
 
 //        создали поле воода сообщения, изменили цвет фона, цвет текста и цвет каретки. Добавили обработку событий
-        TextField tf = new TextField();
+        tf = new TextField();
         tf.setBackground(new Color(43,43, 43));
         tf.setForeground(new Color(169,183,198));
         tf.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String text = tf.getText();
-                ta.setText(text);
+                if (text!=null && !text.isEmpty()){
+                    TextMassage msg = new TextMassage("Вы:", text);
+                    massageListModel.add(massageListModel.getSize(),msg);
+                }
                 tf.setText(null);
             }
         });
@@ -46,13 +69,16 @@ public class ChatWindow extends JFrame {
 
 
 //        создали кнопку отправки сообщения и изменили ее размер. Добавили обработку событий.
-        JButton jb = new JButton("SEND");
+        jb = new JButton("SEND");
         jb.setPreferredSize(new Dimension(120, 80));
         jb.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String text = tf.getText();
-                ta.setText(text);
+                if (text!=null && !text.trim().isEmpty()){
+                    TextMassage msg = new TextMassage("Вы :", text);
+                    massageListModel.add(massageListModel.getSize(),msg);
+                }
                 tf.setText(null);
             }
         });
